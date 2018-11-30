ec189134f550e20f6e8a6c6625232ff7926b84ec
Alex Corn <alexcorn@gmail.com>
2018-11-30 15:28:18 -0500

    Added SEI legal header to all .cs files.

--- RavelloShim/RavelloVirtualMachineUtility.cs
+++ RavelloShim/RavelloVirtualMachineUtility.cs
@@ -1,4 +1,25 @@
-﻿using Newtonsoft.Json;
+// Virtualization Abstraction Layer
+//
+// Copyright 2018 Carnegie Mellon University. All Rights Reserved.
+//
+// NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+//
+// Released under a MIT (SEI)-style license, please see license.txt or contact permission@sei.cmu.edu for full terms.
+//
+// [DISTRIBUTION STATEMENT A] This material has been approved for public release and unlimited distribution.  Please see Copyright notice for non-US Government use and distribution.
+//
+// This Software includes and/or makes use of the following Third-Party Software subject to its own license:
+//
+// 1. Newtonsoft JSON (https://www.newtonsoft.com/json) Copyright 2007 Newtonsoft.
+// 2. RESTSharp (http://restsharp.org/) Copyright 2018 RESTSharp Contributors.
+// 3. VMware SDK (https://www.vmware.com/support/developer/vc-sdk/index.html) Copyright 2018 VMWare, Inc..
+// 4. VirtualBox SDK (https://www.virtualbox.org/sdkref/) Copyright 2006-2010 Oracle.
+// 5. XenServer C# Bindings (https://xenserver.org/open-source-virtualization-download.html) Copyright 1999-2018 Citrix Systems, Inc.
+//
+// DM18-1224
+//
+
+using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Authenticators;
 using System;
